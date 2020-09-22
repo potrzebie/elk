c94a26e11b1a4cd5d1b6d3166e45f410f0f05483
potrzebie <6haker@informatik.uni-hamburg.de>
2020-09-22 13:28:56 +0000

    Filebeat and spec tests

---
+++ manifests/filebeat.pp
@@ -0,0 +1,44 @@
+# elk::filebeat
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include elk::filebeat
+# Filebeat prospectors - expects array of structured data
+# [ {
+#   "type" => "log",
+#   "paths" => [
+#      "/var/log/puppetlabs/puppetserver/puppetserver.log.json",
+#      "/var/log/puppetlabs/puppetserver/puppetserver-access.log.json",
+#    ],
+#   "json_keys_under_root" => true
+# } ] 
+
+class elk::filebeat (
+  $prospectors     = [],
+  $logstash_server = '127.0.0.1',
+  $logstash_port   = '5044',
+){
+  include elastic_stack::repo
+
+  package{'filebeat':
+    ensure => present,
+  }
+  service{'filebeat':
+    ensure  => running,
+    enable  => true,
+    require => Package['filebeat'],
+  }
+  file{'/etc/filebeat/filebeat.yml':
+    ensure => file,
+    content => epp('elk/filebeat.yml.epp',{
+      prospectors     => $prospectors,
+      logstash_server => $logstash_server,
+      logstash_port   => $logstash_port,
+      })
+    }
+}
+
+
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,32 @@
-# @summary A short summary of the purpose of this class
+# elk
 #
 # A description of what this class does
+#
+# @summary A short summary of the purpose of this class
 #
 # @example
 #   include elk
 class elk {
+  class {'logstash':}
+  logstash::plugin {'logstash-input-beats':}
+  logstash::configfile{'beats':
+    source => 'puppet:///modules/elk/beats.conf',
+  }
+
+
+  include ::java
+  class { 'elasticsearch':
+    jvm_options => ['-Xms256m','-Xmx256m']  
+  }
+  elasticsearch::instance { 'es-01': }
+
+  class {'kibana': 
+    config => {
+      'server.host' => '0.0.0.0'
+    }
+  }
+
+
+  include elk::filebeat
 }
+
